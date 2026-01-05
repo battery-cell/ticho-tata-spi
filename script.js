d493fa391cd5d251be1b8c4e66e34885bfeac7b0
Jakub Obrębski <jakub.obrebski@infotech.edu.pl>
2026-01-05 12:44:49 +0100

    tylko nie rob konfliktow

--- script.js
+++ script.js
@@ -11,5 +11,18 @@ function updateTime() {
     document.getElementById("time").textContent = hours + ":" + minutes + ":" + seconds;
 }
 
+function updateMode() {
+    const now = new Date();
+    const hour = now.getHours();
+
+    if (hour >= 18 || hour < 6) {
+        document.body.classList.add("dark");
+    } else {
+        document.body.classList.remove("dark");
+    }
+}
+
 updateTime();
+updateMode();
 setInterval(updateTime, 1000);
+setInterval(updateMode, 60000);
